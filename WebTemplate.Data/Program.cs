02188288de2bb507f38a99a29474e1591ba5b810
Triple H <tripleh402.dev@gmail.com>
2020-04-27 09:39:19 +0700

    init project

---
+++ WebTemplate.Data/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WebTemplate.Data
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
